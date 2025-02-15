1add3d41f66a84eb2a9525ca0f9d2137df35d0b1
Felipe Guerzoni <161882746+flp2113@users.noreply.github.com>
2025-02-15 10:47:30 -0300

    program definitions

---
+++ src/defs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define APP_TITLE "Algorithm Visualizer"
+#define APP_VERSION "1.0"
+#define APP_IDENTIFIER "com.gz.algorithm-visualizer"
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define WINDOW_TITLE "Algorithm Visualizer"
+#define WINDOW_FLAGS NULL
+
+#define MAX_VALUE_IN_VECTOR 500
+#define DELAY_VECTOR_CHANGES 50
