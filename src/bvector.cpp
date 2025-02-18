9d76bbb0aa368680cd8a12a85ba4c7e020357037
Felipe Guerzoni <161882746+flp2113@users.noreply.github.com>
2025-02-18 20:29:51 -0300

    refactoring bvector

---
+++ src/bvector.cpp
@@ -0,0 +1,175 @@
+#pragma once
+
+#include "bvector.h"
+
+BVector::BVector() noexcept {
+    initialize(MIN_BLOCKS_IN_VECTOR);
+}
+
+BVector::BVector(const Sint32 numberOfBlocks) noexcept {
+    initialize(MIN_BLOCKS_IN_VECTOR);
+}
+
+BVector::BVector(const std::vector<Block> v) noexcept : vec(v) {
+    toArray();
+    bubbleSort();
+}
+
+Block BVector::getBlock(const Sint32 index) const {
+    if (isInvalidIndex(index))
+        throw std::out_of_range("GET BLOCK: OUT OF BOUNDS INDEX");
+
+    return vec.at(index);
+}
+
+void BVector::setBlock(const Sint32 index, const Block block) {
+    if (isInvalidIndex(index))
+        throw std::out_of_range("SET BLOCK: OUT OF BOUNDS INDEX");
+
+    vec.at(index) = block;
+}
+
+size_t BVector::getSize() const noexcept {
+    return vec.size();
+}
+
+void BVector::resize(const size_t size) {
+    if (size > UINT32_MAX)
+        throw std::out_of_range("SET SIZE: INVALID SIZE");
+
+    vec.resize(size);
+    delete[] rects;
+    rects = nullptr;
+
+    while (!steps.empty()) {
+        steps.pop();
+    }
+
+    initialize(size);
+}
+
+bool BVector::isEmpty() const noexcept {
+    return vec.size() == 0;
+}
+
+void BVector::toArray() noexcept {
+    size_t vecSize = vec.size();
+    rects = new SDL_FRect[vecSize];
+    for (size_t i = 0; i < vecSize; i++) {
+        rects[i] = vec.at(i).rect;
+    }
+}
+
+void BVector::fillBlocks(const Sint32 numberOfBlocks) noexcept {
+    vec.clear();
+
+    srand(time(NULL));
+
+    for (Sint32 i = 0; i < numberOfBlocks; i++) {
+        float blockValue = (rand() % MAX_VALUE_IN_VECTOR) + 1;
+        Block block = {
+            i,
+            blockValue,
+            {
+                i * (WINDOW_WIDTH / getSize()), WINDOW_HEIGHT - blockValue,
+                WINDOW_WIDTH / getSize(), blockValue
+            },
+            false
+        };
+
+        vec.push_back(block);
+    }
+}
+
+void BVector::renderBlocks(SDL_Renderer* renderer) noexcept {
+    for (Uint32 i = 0; i < vec.size(); i++) {
+        rects[i] = vec.at(i).rect;
+        setBlockDimensions(vec.at(i), i);
+
+        if (vec.at(i).targeted)
+            SDL_SetRenderDrawColorFloat(renderer, 1.0f, 0.0f, 0.0f, SDL_ALPHA_OPAQUE);
+        else
+            SDL_SetRenderDrawColorFloat(renderer, 1.0f, 1.0f, 1.0f, SDL_ALPHA_OPAQUE);
+
+        SDL_RenderFillRect(renderer, &rects[i]);
+    }
+
+}
+
+void BVector::renderSortSteps(SDL_Renderer* renderer, const Sint32 delay) noexcept {
+    if (steps.empty()) {
+        SDL_RenderPresent(renderer);
+        return;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_RenderDebugText(renderer, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, std::to_string(steps.size()).c_str());
+
+    std::tuple<Uint32, Uint32> step = steps.front();
+
+    Uint32 firstIndex = std::get<0>(step);
+    Uint32 secondIndex = std::get<1>(step);
+
+    switchColors(vec.at(firstIndex));
+    switchColors(vec.at(secondIndex));
+
+    std::swap(vec.at(firstIndex), vec.at(secondIndex));
+    std::swap(rects[firstIndex], rects[secondIndex]);
+
+    renderBlocks(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(delay);
+
+    switchColors(vec.at(firstIndex));
+    switchColors(vec.at(secondIndex));
+
+    steps.pop();
+}
+
+void BVector::bubbleSort() noexcept {
+    std::vector<Block> vec_tmp = vec;
+    size_t n = vec.size();
+    bool swapped;
+
+    for (Uint32 i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (Uint32 j = 0; j < n - i - 1; j++) {
+            if (vec_tmp.at(j).value > vec_tmp.at(j + 1).value) {
+                std::swap(vec_tmp.at(j), vec_tmp.at(j + 1));
+
+                steps.push(std::make_tuple(j, j + 1));
+
+                swapped = true;
+            }
+        }
+
+        if (!swapped) 
+            break;
+    }
+}
+
+bool BVector::isInvalidIndex(const Sint32 index) const noexcept {
+    return index < 0 || index > vec.size() - 1;
+}
+
+void BVector::setBlockDimensions(Block& block, const Sint32 index) const noexcept {
+    block.rect.h = block.value;
+    block.rect.w = WINDOW_WIDTH / static_cast<float>(getSize());
+
+    block.rect.x = index * block.rect.w;
+    block.rect.y = WINDOW_HEIGHT - block.value;
+}
+
+void BVector::switchColors(Block& block) const noexcept {
+    block.targeted = !block.targeted;
+}
+
+void BVector::initialize(const Sint32 numberOfBlocks) noexcept {
+    fillBlocks(numberOfBlocks);
+    toArray();
+    bubbleSort();
+}
+
+
--- src/bvector.h
+++ src/bvector.h
@@ -1,222 +1,53 @@
 #pragma once
 
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 
-#include "defs.h"
-
+#include <stdexcept>
+#include <iostream>
+#include <cstdlib>
+#include <string>
 #include <vector>
 #include <queue>
 #include <tuple>
-#include <stdexcept>
-#include <cstdlib>
-#include <iostream>
-#include <string>
 #include <ctime>
 
-#define DEBUG
+#include "defs.h"
 
-namespace GZBlock {
-    typedef struct {
-        Uint32 index;
-        float value;
-        SDL_FRect rect;
-        bool targeted;
-    } Block;
+typedef struct {
+    Sint32 index;
+    float value;
+    SDL_FRect rect;
+    bool targeted;
+} Block;
 
-    class BVector {
-    public:
-        SDL_FRect* rects;
-        std::vector<Block> vec;
-        std::queue<std::tuple<Uint32, Uint32>> steps;
-        
-        BVector(Uint32 numberOfBlocks) {
-            fillBlocks(numberOfBlocks);
-            toArray();
-            bubbleSort();
-        }
+typedef struct {
+    float r, g, b, a;
+} Color;
 
-        BVector(std::vector<Block> v) : vec(v) {
-            toArray();
-            bubbleSort();
-        }
+class BVector {
+public:
+    SDL_FRect* rects;
+    std::vector<Block> vec;
+    std::queue<std::tuple<Sint32, Sint32>> steps;
 
-        Block getBlock(Uint32 index) {
-            if (isInvalidIndex(index))
-                throw std::out_of_range("GET BLOCK: OUT OF BOUNDS INDEX");
+    BVector                 ()                              noexcept;
+    explicit BVector        (const Sint32)                  noexcept;
+    explicit BVector        (const std::vector<Block>)      noexcept;
 
-            Block block = vec.at(index);
+    void resize             (const size_t);
+    void fillBlocks         (const Sint32)                  noexcept;
+    void renderBlocks       (SDL_Renderer*)                 noexcept;
+    void renderSortSteps    (SDL_Renderer*, const Sint32)   noexcept;
+private:
+    size_t getSize          ()                              const noexcept;
+    bool isEmpty            ()                              const noexcept;
+    bool isInvalidIndex     (const Sint32)                  const noexcept;
+    void setBlockDimensions (Block&, const Sint32)          const noexcept;
+    void switchColors       (Block&)                        const noexcept;
+    void initialize         (const Sint32)                  noexcept;
+    void toArray            ()                              noexcept;
+    void bubbleSort         ()                              noexcept;
+    Block getBlock          (const Sint32)                  const;
+    void setBlock           (const Sint32, const Block);
+};
 
-            #ifdef DEBUG
-                std::cout << "(DEBUG) GET BLOCK: "
-                    << "[" << block.index << ", "
-                    << block.value << ", "
-                    << "{ " << block.rect.x << ", " << block.rect.y << ", " << block.rect.w << ", " << block.rect.h << " }, "
-                    << (block.targeted ? "true" : "false") << "]" << std::endl;
-            #endif
-
-            return block;
-        }
-
-        void setBlock(Uint32 index, Block block) {
-            if (isInvalidIndex(index))
-                throw std::out_of_range("SET BLOCK: OUT OF BOUNDS INDEX");
-
-            Block toChange = vec.at(index);
-
-            #ifdef DEBUG
-                std::cout << "(DEBUG) SET BLOCK: "
-                    << "[" << toChange.index << ", "
-                    << toChange.value << ", "
-                    << "{ " << toChange.rect.x << ", " << toChange.rect.y << ", " << toChange.rect.w << ", " << toChange.rect.h << " }, "
-                    << (toChange.targeted ? "true" : "false") << "] ===> "
-                    << "[" << block.index << ", "
-                    << block.value << ", "
-                    << "{ " << block.rect.x << ", " << block.rect.y << ", " << block.rect.w << ", " << block.rect.h << " }, "
-                    << (block.targeted ? "true" : "false") << "]" << std::endl;
-            #endif
-
-            vec.at(index) = block;
-        }
-
-        size_t getSize() {
-            return vec.size();
-        }
-
-        void setSize(size_t size) {
-            if (size > UINT32_MAX)
-                throw std::out_of_range("SET SIZE: INVALID SIZE");
-
-            vec.resize(size);
-            delete[] rects;
-            rects = nullptr;
-
-            while (!steps.empty()) {
-                steps.pop();
-            }
-
-            fillBlocks(size);
-            toArray();
-            bubbleSort();
-        }
-
-        bool isEmpty() {
-            return vec.size() == 0;
-        }
-
-        void renderBlocks(SDL_Renderer* renderer) {
-            for (Uint32 i = 0; i < vec.size(); i++) {
-                rects[i] = vec.at(i).rect;
-                setBlockDimensions(vec.at(i), i);
-                
-                if (vec.at(i).targeted)
-                    SDL_SetRenderDrawColorFloat(renderer, 1.0f, 0.0f, 0.0f, SDL_ALPHA_OPAQUE);
-                else
-                    SDL_SetRenderDrawColorFloat(renderer, 1.0f, 1.0f, 1.0f, SDL_ALPHA_OPAQUE);
-
-                SDL_RenderFillRect(renderer, &rects[i]);
-            }
-        }
-
-        void toArray() {
-            size_t vecSize = vec.size();
-            rects = new SDL_FRect[vecSize];
-            for (size_t i = 0; i < vecSize; i++) {
-                rects[i] = vec.at(i).rect;
-            }
-        }
-
-        void bubbleSort() {
-            std::vector<Block> vec_tmp = vec;
-            size_t n = vec.size();
-            bool swapped;
-
-            for (Uint32 i = 0; i < n - 1; i++) {
-                swapped = false;
-                for (Uint32 j = 0; j < n - i - 1; j++) {
-                    if (vec_tmp.at(j).value > vec_tmp.at(j + 1).value) {
-                        std::swap(vec_tmp.at(j), vec_tmp.at(j + 1));
-
-                        steps.push(std::make_tuple(j, j + 1));
-
-                        swapped = true;
-                    }
-                }
-
-                if (!swapped) break;
-            }
-        }
-
-        void renderSortSteps(SDL_Renderer* renderer) {
-            if (steps.empty()) {
-                SDL_RenderPresent(renderer);
-                return;
-            }
-
-            //std::cout << steps.size() << '\n'; //DEBUG STEPS
-
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, std::to_string(steps.size()).c_str());
-
-            std::tuple<Uint32, Uint32> step = steps.front();
-
-            Uint32 firstIndex = std::get<0>(step);
-            Uint32 secondIndex = std::get<1>(step);
-
-            switchColors(vec.at(firstIndex));
-            switchColors(vec.at(secondIndex));
-
-            std::swap(vec.at(firstIndex), vec.at(secondIndex));
-            std::swap(rects[firstIndex], rects[secondIndex]);
-
-            renderBlocks(renderer);
-
-            SDL_RenderPresent(renderer);
-
-            SDL_Delay(DELAY_VECTOR_CHANGES);
-
-            switchColors(vec.at(firstIndex));
-            switchColors(vec.at(secondIndex));
-
-            steps.pop();
-        }
-
-        void fillBlocks(Uint32 numberOfBlocks) {
-            vec.clear();
-
-            srand(time(NULL));
-
-            for (Uint32 i = 0; i < numberOfBlocks; i++) {
-                float blockValue = (rand() % MAX_VALUE_IN_VECTOR) + 1;
-                Block block = { // { index, value, SDL_FRect, targeted }
-                    i,
-                    blockValue,
-                    {//xywh
-                        i * (WINDOW_WIDTH / getSize()), WINDOW_HEIGHT - blockValue,
-                        WINDOW_WIDTH / getSize(), blockValue
-                    },
-                    false
-                };
-
-                vec.push_back(block);
-            }
-        }
-
-        private:
-            bool isInvalidIndex(Uint32 index) {
-                return index < 0 || index > vec.size() - 1;
-            }
-
-            void setBlockDimensions(Block& block, Uint32 index) {
-                block.rect.h = block.value;
-                block.rect.w = WINDOW_WIDTH / static_cast<float>(getSize());
-
-                block.rect.x = index * block.rect.w;
-                block.rect.y = WINDOW_HEIGHT - block.value;
-            }
-
-            void switchColors(Block& block) {
-                block.targeted = !block.targeted;
-            }
-    };
-}
