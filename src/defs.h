9fd65613d954e956ee4506f54dc2f4620b1e619e
Felipe Guerzoni <161882746+flp2113@users.noreply.github.com>
2025-03-01 16:33:07 -0300

    minor change

--- src/defs.h
+++ src/defs.h
@@ -1,24 +1,24 @@
 #pragma once
 
-#define APP_TITLE				"Algorithm Visualizer"
-#define APP_VERSION				"1.0"
-#define APP_IDENTIFIER			"com.gz.algorithm-visualizer"
+#define APP_TITLE                   "Algorithm Visualizer"
+#define APP_VERSION	                "1.0"
+#define APP_IDENTIFIER              "com.gz.algorithm-visualizer"
 
-#define WINDOW_WIDTH			1280.0f
-#define WINDOW_HEIGHT			720.0f
-#define WINDOW_TITLE			"Algorithm Visualizer"
-#define WINDOW_FLAGS			NULL
+#define WINDOW_WIDTH                1280.0f
+#define WINDOW_HEIGHT               720.0f
+#define WINDOW_TITLE                "Algorithm Visualizer"
+#define WINDOW_FLAGS                NULL
 
-#define GUI_TITLE				"Algorithm Visualizer"
-#define GUI_WIDTH				400.0f
-#define GUI_HEIGHT				200.0f
-#define GUI_X					0.0f
-#define GUI_Y					0.0f
-#define GUI_FLAGS NULL
-#define GUI_WINDOW_FLAGS		(ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove)
+#define GUI_TITLE                   "Algorithm Visualizer"
+#define GUI_WIDTH                   400.0f
+#define GUI_HEIGHT                  200.0f
+#define GUI_X                       0.0f
+#define GUI_Y                       0.0f
+#define GUI_FLAGS                   NULL
+#define GUI_WINDOW_FLAGS            (ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove)
 
-#define MAX_VALUE_IN_VECTOR		500
-#define MIN_BLOCKS_IN_VECTOR	25
-#define MAX_BLOCKS_IN_VECTOR	500
-#define MIN_DELAY				15
-#define MAX_DELAY				150
+#define MAX_VALUE_IN_VECTOR	        500
+#define MIN_BLOCKS_IN_VECTOR        25
+#define MAX_BLOCKS_IN_VECTOR        500
+#define MIN_DELAY                   15
+#define MAX_DELAY                   150
